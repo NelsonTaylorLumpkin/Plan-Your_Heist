7e6f9c9287424d927013c9a87935d2946b9e7850
Nelson Taylor Lumpkin <ossocasa@gmail.com>
2023-03-02 15:35:31 -0600

    phase one complete

--- plan-your-heist/Program.cs
+++ plan-your-heist/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Heist
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan your heist!!!");
+            Console.WriteLine("Choose Your Ride-Or-Die!!!");
+            string name = Console.ReadLine();
+            Console.WriteLine("How good they is?");
+            int skill = int.Parse(Console.ReadLine());
+            Console.WriteLine("How Not Scared Are They?");
+            double courage = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{name} {skill} {courage}");
+
+        }
+    }
+}
